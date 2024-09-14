b8e8e4944b0589f8c3bd168287e18c23f98a4ea8
Marx Alonso <marxchip99@gmail.com>
2024-09-14 14:06:51 -0500

    Cambios general, agregando nuevos apartados

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,19 @@
+import { Banner } from "../../common/Banner/Banner";
 import { Footer } from "../../common/Footer/Footer";
 import { Hero } from "../../common/Hero/Hero";
 import { Navbar } from "../../common/Navbar/Navbar";
 import { Productos } from "../../common/Productos/Productos";
+import { ProductosLista } from "../../common/ProductosLista/ProductosLista";
+import { Testimonios } from "../../common/Testimonios/Testimonios";
 export const HomePage = () => {
     return (
     <>
       <Navbar />
       <Hero />
       <Productos />
+      <ProductosLista />
+      <Testimonios />
+      <Banner />
       <Footer />
     </>
   );
